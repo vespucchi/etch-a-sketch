c5e837076e707f46d22c43cd771cc4b0ddc77ef0
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-03 17:50:03 +0200

    Add grid size button and update JS to remove/add elements

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
-const grid = document.querySelector(".container");
-console.log(grid.style);
+// let body = document.querySelector("body");
+let grid = document.querySelector(".container");
+let button = document.querySelector("button");
+let squares;
 let gridSize = 16;
 
-// populate container with X grid of square divs
-for(let i = 0; i < gridSize * gridSize; i++) {
-    let squareDiv = document.createElement("div");
-    squareDiv.classList.add("grid");
+createGrid(gridSize);
 
-    // Calculate square size with formula
-    //      padding = container width / gridSize / 2
-    //      divide by 2 because padding is set to both sides
+button.addEventListener("click", () => {
+    gridSize = prompt("Grid row size: ")
+    grid.remove();
+    grid = document.createElement("div");
+    grid.classList.add("container");
+    button.parentNode.insertBefore(grid, button.nextSibling)
 
-    squareDiv.style.padding = `calc(32vw / ${gridSize} / 2)`;
-
-    grid.appendChild(squareDiv);
-}
-
-
-// Coloring effect by hovering
-let squares = document.querySelectorAll(".grid");
-squares.forEach((square) => {
-    square.addEventListener("mouseenter", () => {
-        square.style.backgroundColor = "black";
-    })
+    createGrid(gridSize);
 })
+
+
+// populate container with X grid of square divs
+function createGrid(size) {
+    for(let i = 0; i < gridSize * gridSize; i++) {
+        let squareDiv = document.createElement("div");
+        squareDiv.classList.add("grid");
+    
+        // Calculate square size with formula
+        //      padding = container width / gridSize / 2
+        //      divide by 2 because padding is set to both sides
+    
+        squareDiv.style.padding = `calc(32vw / ${size} / 2)`;
+    
+        grid.appendChild(squareDiv);
+    }
+
+    // Coloring effect by hovering
+    squares = document.querySelectorAll(".grid");
+    squares.forEach((square) => {
+        square.addEventListener("mouseenter", () => {
+            square.style.backgroundColor = "black";
+        })
+    })
+}
