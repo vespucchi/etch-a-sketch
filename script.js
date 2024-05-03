056a17777475a219c02ea804b9985c153bcb0d71
vespucchi <56511717+vespucchi@users.noreply.github.com>
2024-05-03 17:10:07 +0200

    Set up container and calculate grid square sizing

---
+++ script.js
@@ -0,0 +1,17 @@
+const grid = document.querySelector(".container");
+console.log(grid.style);
+let gridSize = 16;
+
+// populate container with X grid of square divs
+for(let i = 0; i < gridSize * gridSize; i++) {
+    let squareDiv = document.createElement("div");
+    squareDiv.classList.add("grid");
+
+    // Calculate square size with formula
+    //      padding = container width / gridSize / 2
+    //      divide by 2 because padding is set to both sides
+
+    squareDiv.style.padding = `calc(32vw / ${gridSize} / 2)`;
+
+    grid.appendChild(squareDiv);
+}
